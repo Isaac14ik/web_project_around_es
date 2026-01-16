cc099f41a9cbf7f619b4686b6c8bacf2f417ca78
Isaac14ik <89858013+Isaac14ik@users.noreply.github.com>
2026-01-16 08:06:48 -0600

    Agregue funciones correspondientes y las llame

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 const initialCards = [
-
-{
+  {
     name: "Valle de Yosemite",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/es/yosemite.jpg"
   },
@@ -26,12 +25,45 @@ const initialCards = [
   }
 ];
 
-initialCards.forEach((card) => {
-  
-console.log(card.name);
-})
-
-const modalProfile = document.querySelector('.popup'); 
-
+const modalProfile = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
+const profileForm = document.querySelector('.popup__form');
+
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_job');
+
+function openModal(modal) {
+  modal.classList.add('popup_opened');
+}
+
+function closeModal(modal) {
+  modal.classList.remove('popup_opened');
+}
+
+function fillProfileForm() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal(modalProfile);
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closeModal(modalProfile);
+}
+
+editButton.addEventListener('click', handleOpenEditModal);
+closeButton.addEventListener('click', () => closeModal(modalProfile));
+profileForm.addEventListener('submit', handleProfileFormSubmit);
+
+initialCards.forEach((card) => {
+  console.log(card.name);
+});
